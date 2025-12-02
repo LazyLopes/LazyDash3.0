449843c461fa828b69065d4e2c95d6981c3e01b1
LazyLopes <raphael130904@gmail.com>
2025-12-02 19:15:03 -0300

    coisa ai

--- LazyDash/LazyDash/settings.py
+++ LazyDash/LazyDash/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'LazyDash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lazydb',
+        'USER': 'postgres',
+        'PASSWORD': 'Rabibs110422*',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
