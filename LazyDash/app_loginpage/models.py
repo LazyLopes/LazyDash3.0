1a70988456e1c597fc2683e39963fc578ec4400c
LazyLopes <raphael130904@gmail.com>
2025-12-13 04:25:25 -0300

    Criando o front
    - utilizei o bootstrap pra criar um html
    - botei algumas funcionalidades no views
    - fiz um front de login e senha, ta escrito login mas é mais cadastro mesmo
    
    - foi mexido, urls, models, views, login.html e outras paradas ai

--- LazyDash/app_loginpage/models.py
+++ LazyDash/app_loginpage/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    login = models.CharField(max_length=150, unique=True)
+    senha = models.CharField(max_length=128)
--- LazyDash/app_loginpage/views.py
+++ LazyDash/app_loginpage/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Users
 
 def login (request):
-    return HttpResponse("Página de Login")
+    print(request.method)
+    if request.method == 'GET':
+        status = request.GET.get('status')
+        return render(request, 'login.html', {'status': status})
+    elif request.method == 'POST':
+        login = request.POST.get('login')
+        senha = request.POST.get('senha')
+        
+        # variavel de verificação se o usuário já existe
+        user_exists = Users.objects.filter(login=login).exists()
+        
+        # se o usuário já existe, redireciona para a página de login com status de erro
+        if user_exists:
+            return redirect('/loginpage/login/?status=0')
+        
+        users = Users(
+            login=login,
+            senha=senha
+        )
+        users.save()
+        
+        return HttpResponse(f"Login: {login} | Senha: {senha}")
